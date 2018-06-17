02680d81fd14eb73bad899d8622908937165c113
Paulina Szelemetko <p.szelemetko@gmail.com>
2018-06-17 22:40:13 +0200

    two cat version

--- app.js
+++ app.js
@@ -1,12 +1,41 @@
+function Cat(name, url) {
+    this.name = name;
+    this.clicks = 0;
+    this.url = url;
 
-var clickCounter = {
-    clicks: 0,
-    increment: function() {
-        this.clicks++;
-        document.getElementById("display").innerHTML = this.clicks;
-    }
+    this.drawDivCat();    
 }
 
-$('#photoCat').click(function() {
-    clickCounter.increment();
-  });
+Cat.prototype.increment = function() {
+    this.clicks++;
+    document.getElementById(this.spanId).innerHTML = this.clicks;
+}
+
+Cat.prototype.drawDivCat = function() {
+    let el = document.createElement("div");
+
+    let photo = document.createElement("img");
+    photo.setAttribute("src", this.url);
+
+    let counter = document.createElement("span");
+    counter.innerHTML = this.clicks; 
+
+    el.appendChild(photo); 
+    el.appendChild(counter); 
+
+    document.querySelector(".cats").appendChild(el); //wstawiamy element do drzewa dokumentu
+
+    let _this = this;
+
+    photo.addEventListener("click", function() {
+        _this.clicks++;
+        counter.innerHTML = _this.clicks;
+    });
+
+}
+
+
+let szarek = new Cat("Szarek", "img/cat.jpg"); 
+let miauczek = new Cat("Miauczek", "img/cat2.jpg"); 
+
+
